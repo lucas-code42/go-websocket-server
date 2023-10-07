9b57aa7c6ecc3d06f164f3be0f7593b095f4b1ba
Lucas <lucasdearaujo.brandao@gmail.com>
2023-10-07 20:19:11 -0300

    dmMessage +

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/lucas-code42/web-socket-server
 
 go 1.20
 
-require nhooyr.io/websocket v1.8.7
+require (
+	github.com/google/uuid v1.3.1
+	nhooyr.io/websocket v1.8.7
+)
 
 require github.com/klauspost/compress v1.10.3 // indirect
--- main.go
+++ main.go
@@ -1,74 +1,134 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/google/uuid"
 	"nhooyr.io/websocket"
 	"nhooyr.io/websocket/wsjson"
 )
 
-func wsHandleText(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("wsHandleText")
-	c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
-		InsecureSkipVerify: true,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer c.Close(websocket.StatusInternalError, "closing ws connection")
+var (
+	clients       map[string]*ClientMetaData = make(map[string]*ClientMetaData)
+	globalContext                            = context.Background()
+)
 
-	// MessageType represents the type of a WebSocket message.
-	// See https://tools.ietf.org/html/rfc6455#section-5.6
-	msgType, msg, err := c.Read(r.Context())
-	if err != nil {
-		log.Fatal("error to read message")
-		return
-	}
-	log.Printf("msg type is: %v", msgType)
-	log.Printf("the msg is: %s", string(msg))
+type ClientMetaData struct {
+	NickName            string          `json:"nickname"`
+	Msg                 string          `json:"msg"`
+	Target              string          `json:"target"`
+	Status              string          `json:"status"`
+	WebSocketConnection *websocket.Conn `json:"omitempty"`
 }
 
-func wsHandleJson(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("wsHandleJson")
-	c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
+func (m *ClientMetaData) dmMessage() {
+	if len(clients) > 0 {
+		for _, v := range clients {
+			if (m.Target == v.NickName) && (v.Status == "online") {
+				v.WebSocketConnection.Write(globalContext, websocket.MessageText, []byte(m.Msg))
+				return
+			}
+		}
+		m.WebSocketConnection.Write(globalContext, websocket.MessageText, []byte("coul'd not find your target"))
+	}
+}
+
+func wsHandle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("[*] WEB-SCOKET HANDLE STARTED")
+
+	newConn, err := websocket.Accept(w, r, &websocket.AcceptOptions{
 		InsecureSkipVerify: true,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	type wsJson struct {
-		Msg      string
-		Protocol string
-		Status   string
-	}
-	var v wsJson
+	var newClient ClientMetaData
+	newClient.NickName = r.URL.Query().Get("nickname")
+	newClient.WebSocketConnection = newConn
+	newClient.Status = "online"
+
+	var clientId string = uuid.NewString()
+	clients[clientId] = &newClient
+
+	log.Printf("[*] new client add: %v", newClient.NickName)
+	log.Printf("[*] total clients: %d", len(clients))
+
+	// lifecycle for newConn
 	for {
-		err = wsjson.Read(r.Context(), c, &v)
+		err = wsjson.Read(globalContext, newConn, &newClient)
 		if err != nil {
 			break
 		}
-		log.Printf("received: %v", v)
 
-		if v.Status == "close" {
-			c.Write(r.Context(), websocket.MessageText, []byte("bye bye"))
-			c.Close(websocket.StatusProtocolError, "close connection")
+		if newClient.Status == "offline" {
+			newConn.Write(globalContext, websocket.MessageText, []byte("bye bye"))
+			newConn.Close(websocket.StatusProtocolError, "close connection")
+			delete(clients, clientId)
 			break
 		} else {
-			c.Write(r.Context(), websocket.MessageText, []byte("we still connected"))
+			newClient.dmMessage()
 		}
 	}
-
+	log.Println("[*] close handler for id:" + clientId)
 }
 
+// a schedulle job job to send every connected user
+func showAllClientsConnected() {
+	for {
+		if len(clients) > 0 {
+			time.Sleep(40 * time.Second)
+			for _, v := range clients {
+				jsonRes, err := json.Marshal(clients)
+				if err != nil {
+					v.WebSocketConnection.Write(globalContext, websocket.MessageText, []byte("could not send all clients connected"))
+				}
+				v.WebSocketConnection.Write(globalContext, websocket.MessageText, jsonRes)
+			}
+		}
+	}
+}
+
+// func wsHandleText(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("wsHandleText")
+// 	c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
+// 		InsecureSkipVerify: true,
+// 	})
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer c.Close(websocket.StatusInternalError, "closing ws connection")
+
+// 	// MessageType represents the type of a WebSocket message.
+// 	// See https://tools.ietf.org/html/rfc6455#section-5.6
+// 	msgType, msg, err := c.Read(r.Context())
+// 	if err != nil {
+// 		log.Fatal("error to read message")
+// 		return
+// 	}
+// 	log.Printf("msg type is: %v", msgType)
+// 	log.Printf("the msg is: %s", string(msg))
+// }
+
 func main() {
-	fmt.Println("Initialize WS-server")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello"))
+	log.Println("[*] initialize ws-server")
+
+	go showAllClientsConnected()
+
+	// http.HandleFunc("/ws-text", wsHandleText)
+	http.HandleFunc("/clients", func(w http.ResponseWriter, r *http.Request) {
+		var res []*ClientMetaData
+		for _, v := range clients {
+			res = append(res, v)
+		}
+		log.Println("[*] client user-agent" + r.UserAgent())
+		json.NewEncoder(w).Encode(res)
 	})
-	http.HandleFunc("/ws-text", wsHandleText)
-	http.HandleFunc("/ws-json", wsHandleJson)
+	http.HandleFunc("/ws-json", wsHandle)
 	http.ListenAndServe(":8080", nil)
 }
